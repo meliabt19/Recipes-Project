563221e4e3c1d618933f24cc17468038b9b58fce
Tevin <tevinward7@gmail.com>
2020-05-28 18:24:15 -0400

    made basic html layout of advanced search

--- public/js/advanced-search.js
+++ public/js/advanced-search.js
@@ -1,10 +1,19 @@
 const API_KEY = '520acba345fb4fc582e4496d65f38cef';
 
 $(document).ready(() => {
-  const intolerances = [];
 
+  const dish = $('#dish').val();
+
+  const diet = '';
+
+  const intolerances = ['peanut'];
+  intolOptions = 'soy';
   intolerances.push(intolOptions);
 
-  const query = `https://api.spoonacular.com/recipes/complexSearch?query=chocolate&number=10&maxSugar=40&intolerances=${intolerances}&apiKey=${API_KEY}`;
 
+  const query = `https://api.spoonacular.com/recipes/complexSearch?query=${dish}&diet=${diet}&intolerances=${intolerances}&apiKey=${API_KEY}`;
+
+
+  console.log(query);
+  console.log(dish);
 });
