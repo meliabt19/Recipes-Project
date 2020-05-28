d929d111cc73dd36c6f708e0a77b769f66402707
Nicholas Romano <njr7romano@yahoo.com>
2020-05-28 17:33:55 -0400

    Nick branch (#4)
    * Added name fields in signup form, added validation
    
    * Added Global search query
    
    * Added html template
    
    * Completed global Search front-end

--- public/js/members.js
+++ public/js/members.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
 
   $('#global-search').on('submit', function(event) {
     event.preventDefault();
+    
+    $('#search-results').empty();
+
     const searchInput = $('#search-query').val();
 
     const validInput = validateSearchInput(searchInput);
@@ -48,24 +51,44 @@ $(document).ready(function() {
 });
 
 const createRecipeCard = (recipe) => {
-  const { id, title, readyInMinutes, servings, sourceUrl } = recipe;
 
-  const image = `${title}-${id}-500x500.jpg`;
+  const { id, title, image, readyInMinutes, servings, sourceUrl } = recipe;
 
-  return `<div class="card" style="width: 18rem;">
-            <img src="${image}" class="card-img-top" alt="${title}">
-            <div class="card-body">
-              <h5 class="card-title">${title}</h5>
-              <p class="card-text">Prep Time: ${readyInMinutes}</p>
-              <p class="card-text">Servings: ${servings}</p>
-            </div>
-            <div class="card-body">
-              <a href="#" id="${id}" addToRecipeBook(id) class="card-link">Add</a>
-              <a href="#" class="card-link">Details</a>
-              <a href="${sourceUrl}">Source Link</a>
+  const imageTypeIndex = image.lastIndexOf('.', image.length);
+
+  const imageType = image.substring(imageTypeIndex);
+
+  const imageSize = '480x360';
+
+  const imagePath = `https://spoonacular.com/recipeImages/${id}-${imageSize}${imageType}`;
+
+  return `<div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+            <div class="card">
+              <img src="${imagePath}" class="card-img-top img-fluid" alt="${title}">
+              <div class="card-body">
+                <h4 class="card-title">${title}</h4>
+                <p class="card-text">Prep Time: ${readyInMinutes} minutes</p>
+                <p class="card-text">Servings: ${servings}</p>
+              </div>
+              <div class="card-body">
+                <a href="#" name="${id}" onclick="addToRecipeBook(name)" class="btn btn-primary">Add</a>
+                <a href="#" name="${id}" onclick="viewRecipeDetails(name)" class="btn btn-primary">Details</a>
+                <p><a href="${sourceUrl}">View Source Link</a></p>
+              </div>
             </div>
           </div>`;
 
+};
+
+
+const addToRecipeBook = id => {
+  event.preventDefault();
+  console.log(id);
+};
+
+const viewRecipeDetails = id => {
+  event.preventDefault();
+  console.log(id);
 };
 
 const validateSearchInput = input => {
