e1f75bb8ed1d1bdddfb639ab5f0a16b2cf5f0cbb
Tevin <tevinward7@gmail.com>
2020-05-28 23:31:35 -0400

    refrenced and used code from nick in member.js to display advanced search results in the same way the standard search results are

--- public/js/advanced-search.js
+++ public/js/advanced-search.js
@@ -2,13 +2,6 @@ const API_KEY = '520acba345fb4fc582e4496d65f38cef';
 
 $(document).ready(() => {
 
-
-
-
-
-
-
-
   $('#advanced-search').on('submit', (event) => {
 
     event.preventDefault();
@@ -30,10 +23,56 @@ $(document).ready(() => {
       url: query,
       success: (data) => {
         console.log(data);
-        console.log(query);
+        const results = data.results;
+
+        results.forEach(rec => {
+          const recipe = createRecipeCard(rec);
+          $('#advanced-search-results').append(recipe);
+        });
+
       }
     });
 
   });
+
+  const createRecipeCard = (recipe) => {
+
+    const { id, title, image, readyInMinutes, servings, sourceUrl } = recipe;
+
+    const imageTypeIndex = image.lastIndexOf('.', image.length);
+
+    const imageType = image.substring(imageTypeIndex);
+
+    const imageSize = '480x360';
+
+    const imagePath = `https://spoonacular.com/recipeImages/${id}-${imageSize}${imageType}`;
+
+    return `<div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+              <div class="card">
+                <img src="${imagePath}" class="card-img-top img-fluid" alt="${title}">
+                <div class="card-body">
+                  <h4 class="card-title">${title}</h4>
+                  <p class="card-text">Prep Time: ${readyInMinutes} minutes</p>
+                  <p class="card-text">Servings: ${servings}</p>
+                </div>
+                <div class="card-body">
+                  <a href="#" name="${id}" onclick="addToRecipeBook(name)" class="btn btn-primary">Add</a>
+                  <a href="#" name="${id}" onclick="viewRecipeDetails(name)" class="btn btn-primary">Details</a>
+                  <p><a href="${sourceUrl}">View Source Link</a></p>
+                </div>
+              </div>
+            </div>`;
+
+  };
+
+  const addToRecipeBook = id => {
+    event.preventDefault();
+    console.log(id);
+  };
+
+  const viewRecipeDetails = id => {
+    event.preventDefault();
+    console.log(id);
+  };
 
 });
