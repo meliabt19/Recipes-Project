5e87fe887b5186077201d13ad6a1f103f3e82fb4
meliabt19 <58869913+meliabt19@users.noreply.github.com>
2020-06-10 16:26:51 -0400

    Heroku fix

--- config/config.js
+++ config/config.js
@@ -1,26 +1,24 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    username: 'lat0ci6r650gv6ri',
-    password: 'xbele488fmzw6eh9',
-    database: 'hrsu19rkzewu9msv',
-    host: 'qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'recipes_db',
+    host: '127.0.0.1',
     dialect: 'mysql'
   },
   test: {
     username: 'root',
-    password: 'soccerkid',
-    database: 'database_test',
+    password: process.env.DB_PASS,
+    database: 'recipes_db',
     host: '127.0.0.1',
     dialect: 'mysql'
   },
   production: {
-    username: 'lat0ci6r650gv6ri',
-    password: 'xbele488fmzw6eh9',
-    database: 'hrsu19rkzewu9msv',
-    host: 'qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    use_env_variable : 'JAWSDB_URL',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'e04wuszrgji220kg',
+    host: 'qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com.0.1',
     dialect: 'mysql'
-
   }
 };
