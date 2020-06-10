1129dbb806cc30a7f31cade07cee3453e1e6c044
meliabt19 <58869913+meliabt19@users.noreply.github.com>
2020-06-10 17:59:32 -0400

    Revert "Britt branch (#29)" (#30)
    This reverts commit 2fe483dd3ac5b6de2a6620ec96767a41779099d9.

--- config/config.js
+++ config/config.js
@@ -15,10 +15,10 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USER,
+    username: 'root',
     password: process.env.DB_PASS,
-    database: 'e04wuszrgji220kg',
-    host: 'qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com.0.1',
+    database: 'recipes_db',
+    host: '127.0.0.1',
     dialect: 'mysql'
   }
 };
